6518efc0541ec4db1bc672ddbe77c6b926ce8a9e
vanrash <ivanderhaidar@gmail.com>
2026-06-02 17:55:46 +0700

    isi Main

---
+++ src/main/java/org/main/MainApp.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainApp {
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        SwingUtilities.invokeLater(() -> {
+            MainFrame frame = new MainFrame();
+            frame.setVisible(true);
+        });
+    }
+}
